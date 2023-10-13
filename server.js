78c247feb17b6eddb035c1beb85e5497f46cc6e6
DavidDuque <105513600+davidduque1992@users.noreply.github.com>
2023-10-13 01:44:56 -0300

    fetat(base): cration of base initial

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser"; //no se instala es de node
+
+var app = express();
+// app.use(
+//   cors({
+//     origin: "http://example.com",
+//   })
+// );
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/ping", (req, res) => {
+  res.status(200).send("pong");
+});
+
+const PORT = 3001;
+app.listen(PORT, () => console.log(`Api corriendo por el puerto ${PORT}`));
