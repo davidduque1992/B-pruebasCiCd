f2e32a7590315ee2c4f528e40d09822bf1c0ce87
DavidDuque <105513600+davidduque1992@users.noreply.github.com>
2023-10-13 18:49:28 -0300

    feat(deploy):creation of route for automatic deploy

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.use(bodyParser.json());
 app.get("/ping", (req, res) => {
   res.status(200).send("pong");
 });
+app.get("/deploy", (req, res) => {
+  res.status(200).send("ejecutar secuencia de comandos del pipeline cicd");
+});
 
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Api corriendo por el puerto ${PORT}`));
