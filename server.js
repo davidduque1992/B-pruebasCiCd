29cae927cc855048673e77a1093ce342329bae93
DavidDuque <105513600+davidduque1992@users.noreply.github.com>
2023-10-16 21:30:33 -0300

    fix(initial):mejora en la estructura del codigo

--- server.js
+++ server.js
@@ -10,49 +10,43 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-async function deploy(req, branch, signature, res) {
+app.post("/app1", async (req, res) => {
+  const branch = req.body.ref.split("/").pop();
+  const signature = req.headers["x-hub-signature"];
+  console.log("secret=", signature);
   const expectedSignature = crypto
     .createHmac("sha1", process.env.GITHUB_WEBHOOK_SECRET)
     .update(JSON.stringify(req.body))
     .digest("hex");
 
-  if (signature !== `sha1=${expectedSignature}`) {
-    console.log(
-      "Se detectó un cambio en la rama ->",
-      branch,
-      "pero la firma no es correcta. Por favor, asegúrate de que estás utilizando la contraseña correcta."
-    );
-    return;
-  }
-
-  try {
-    console.log("Iniciado el proceso de deploy de la rama ->", branch);
-    await executeCommand(
-      "cd /var/www/html/app1 && git fetch && git pull && docker-compose up -d --build"
-    );
-    console.log("Deploy terminado correctamente");
-    res.status(200).send("Deploy terminado correctamente");
-  } catch (error) {
-    console.log(`error: ${error.message}`);
-    res.status(500).send(`Error: ${error.message}`);
-    try {
-      await executeCommand(
-        "cd /var/www/html/app1 && git reset --hard HEAD~1 && docker-compose up -d --build"
-      );
-      console.log(`Rollback realizado correctamente`);
-    } catch (rollbackError) {
-      console.log(`Error durante el rollback: ${rollbackError.message}`);
-    }
-  }
-}
-
-app.post("/app1", async (req, res) => {
-  const branch = req.body.ref.split("/").pop();
-  const signature = req.headers["x-hub-signature"];
-  console.log("secret=", signature);
-
   if (branch === "main") {
-    await deploy(req, branch, signature, res);
+    if (signature === `sha1=${expectedSignature}`) {
+      try {
+        console.log("Iniciado el proceso de deploy de la rama ->", branch);
+        await executeCommand(
+          "cd /var/www/html/app1 && git fetch && git pull && docker-compose up -d --build"
+        );
+        console.log("Deploy terminado correctamente");
+        res.status(200).send("Deploy terminado correctamente");
+      } catch (error) {
+        console.log(`error: ${error.message}`);
+        res.status(500).send(`Error: ${error.message}`);
+        try {
+          await executeCommand(
+            "cd /var/www/html/app1 && git reset --hard HEAD~1 && docker-compose up -d --build"
+          );
+          console.log(`Rollback realizado correctamente`);
+        } catch (rollbackError) {
+          console.log(`Error durante el rollback: ${rollbackError.message}`);
+        }
+      }
+    } else {
+      console.log(
+        "Se detectó un cambio en la rama ->",
+        branch,
+        "pero la firma no es correcta. Por favor, asegúrate de que estás utilizando la contraseña correcta."
+      );
+    }
   } else {
     console.log(
       "se detecto un cambio en la rama ->",
@@ -66,11 +60,8 @@ const executeCommand = (command) => {
   return new Promise((resolve, reject) => {
     exec(command, (error, stdout, stderr) => {
       if (error) {
+        console.log(`stderr: ${stderr}`);
         reject(error);
-        return;
-      }
-      if (stderr) {
-        reject(new Error(stderr));
         return;
       }
       resolve(stdout);
@@ -78,7 +69,7 @@ const executeCommand = (command) => {
   });
 };
 
-const PORT = process.env.PORT || 3001; // Usa el puerto del entorno si está disponible
+const PORT = 3001;
 const server = http.createServer(app);
 server.setTimeout(500000); // Tiempo de espera en milisegundos
 server.listen(PORT, () => console.log(`Api corriendo por el puerto ${PORT}`));
